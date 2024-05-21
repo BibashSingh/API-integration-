ca897a20519c037036dab20f5e0f3b29aad82389
BibashSingh <bibashsingh999@gmail.com>
2024-05-21 19:13:40 +0545

    added model folder

--- lib/Screen/Homepage.dart
+++ lib/Screen/Homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:api/Model/User.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,34 +12,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<dynamic> users = [];
+  List<User> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("API demo app"),
+        title: Text(
+          "API demo app",
+          style: TextStyle(color: Colors.blue),
+        ),
+        centerTitle: true,
       ),
       body: ListView.builder(
           itemCount: users.length,
           itemBuilder: (context, index) {
             final user = users[index];
-            final email = user['email'];
-            final gender=user['gender'];
-            final name=user['name'];
-            final namevitratl = name['title'];
-            final namevitrafn = name['first'];
-            final namevitraln = name['last'];
+            final email = user.email;
+            final gender = user.gender;
+            // final name=user.name;
+            // final namevitratl = name['title'];
+            // final namevitrafn = name['first'];
+            // final namevitraln = name['last'];
+            // final imageUrl=user['picture']['thumbnail'];
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: Card(
-                
                 child: ListTile(
-                  leading: CircleAvatar(child: Text("${index+1}")),
-                  title: Text("${namevitratl} ${namevitrafn} ${namevitraln}",style: TextStyle(fontWeight: FontWeight.bold),),
-                  subtitle: Text(email),
-                  trailing: Text(gender),
-                
-                
+                  // leading: ClipRRect(
+                  //   borderRadius: BorderRadius.circular(100),
+
+                  //   child: Image.network(imageUrl)),
+                  // title: Text("${namevitratl} ${namevitrafn} ${namevitraln}",style: TextStyle(fontWeight: FontWeight.bold),),
+                  subtitle: Text(user.email),
+                  title: Text(user.name.first),
+                  trailing: Text(user.gender),
                 ),
               ),
             );
@@ -57,8 +64,23 @@ class _MyHomePageState extends State<MyHomePage> {
     final response = await http.get(uri);
     final body = response.body;
     final json = jsonDecode(body);
+    final result = json['results'] as List<dynamic>;
+
+    final transform = result.map((e) {
+      final name = Username(
+          title: e['name']['title'],
+          first: e['name']['first'],
+          last: e['name']['last']);
+      return User(
+          gender: e['gender'],
+          email: e['email'],
+          phone: e['phone'],
+          cell: e['cell'],
+          nat: e['nat'],
+          name : name);
+    }).toList();
     setState(() {
-      users = json['results'];
+      users = transform;
     });
     print("second");
   }
